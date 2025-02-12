6b0ce27e58ccb3b8d0f032e26aa42d73ef40be24
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-12 12:02:07 +0700

    Update file paths in save-data.R to use relative paths for data saving and loading

--- 03 Working with Data/save-data.R
+++ 03 Working with Data/save-data.R
@@ -1,14 +1,14 @@
 # Save all environment data into a file
-save.image(file = "../saveData/data.RData")
+save.image(file = "saveData/data.RData")
 
 # Load data from a file to Environment
-load("../saveData/data.RData")
+load("saveData/data.RData")
 
 # Save a specific object to a file
-saveRDS(school, file = "../saveData/school.RDS")
+saveRDS(school, file = "saveData/school.RDS")
 
 # Load a specific object from a file
-readRDS("../saveData/school.RDS")
+readRDS("saveData/school.RDS")
 
 # Add Data into Environment
-school <- readRDS("../saveData/school.RDS")
+school <- readRDS("saveData/school.RDS")
