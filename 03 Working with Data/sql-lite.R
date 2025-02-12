8a95e6764549971897bd44a690c5f7c892d2c6cc
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-12 12:00:47 +0700

    Add SQLite database connection and data retrieval functions, and implement data saving/loading methods

---
+++ 03 Working with Data/sql-lite.R
@@ -0,0 +1,17 @@
+# load library
+library(RSQLite)
+
+# connect to SQLite database (.db file)
+# 1. open connection
+conn <- dbConnect(SQLite(), "data/chinook.db")
+
+# 2. get data
+dbListTables(conn)
+dbListFields(conn, "customers")
+
+# 2.1 get data from table
+df <- dbGetQuery(conn, "select * from customers where country = 'USA'")
+(df2 <- dbGetQuery(conn, "select * from customers where country = 'United Kingdom'"))
+
+# 3. close connection
+dbDisconnect(conn)
