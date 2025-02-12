6a0f9c4e4533550aa2b4e23f40a944d75d8aebbd
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-12 15:26:16 +0700

    Add example for updating vectors in R

---
+++ live/vector.R
@@ -0,0 +1,13 @@
+# Update vector
+gpas <- c(toy = 3.41, bew = 3.95, john = 2.58)
+gpas
+
+gpas["toy"] <- 3.45
+gpas["bew"] <- 4.0
+gpas
+
+gpas2 <- c(jane = 3.78, doe = 3.80)
+gpas2
+
+final_grade <- c(gpas, gpas2)
+final_grade
