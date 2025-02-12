de778241e81de807e5f3d7840d87ffdb85eccb91
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-12 18:17:44 +0700

    Add examples of for and while loops in R

--- homework/pao-ying-chub.R
+++ homework/pao-ying-chub.R
@@ -1,12 +1,14 @@
+# Load the crayon package
+library(crayon)
+
 # Function to play Pao Ying Chub
 pao_ying_chub <- function() {
   options <- c("rock", "paper", "scissors")
+  short_keys <- c("r", "p", "s")
   results <- list(wins = 0, losses = 0, ties = 0)
 
-  play_again <- TRUE
-
-  while (play_again) {
-    cat("Choose rock, paper, or scissors (or type 'exit' to quit): ")
+  while (TRUE) {
+    cat("Choose (r) rock, (p) paper, or (s) scissors (or type 'exit' to quit): ")
     player_choice <- tolower(readline())
 
     if (player_choice == "exit") {
@@ -18,7 +20,9 @@ pao_ying_chub <- function() {
       break
     }
 
-    if (!player_choice %in% options) {
+    if (player_choice %in% short_keys) {
+      player_choice <- options[match(player_choice, short_keys)]
+    } else if (!player_choice %in% options) {
       cat("Invalid choice. Please choose rock, paper, or scissors.\n")
       next
     }
@@ -27,15 +31,15 @@ pao_ying_chub <- function() {
     cat("Computer chose:", computer_choice, "\n")
 
     if (player_choice == computer_choice) {
-      cat("It's a tie!\n")
+      cat(bgYellow("It's a tie!\n"))
       results$ties <- results$ties + 1
     } else if ((player_choice == "rock" && computer_choice == "scissors") ||
       (player_choice == "paper" && computer_choice == "rock") ||
       (player_choice == "scissors" && computer_choice == "paper")) {
-      cat("You win!\n")
+      cat(bgGreen("You win!\n"))
       results$wins <- results$wins + 1
     } else {
-      cat("You lose!\n")
+      cat(bgRed("You lose!\n"))
       results$losses <- results$losses + 1
     }
   }
